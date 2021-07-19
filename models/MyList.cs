eea95fc1755db082a10f170a9fd2cbdc46d698db
Amber Fenimore <amberfenimore@Ambers-MacBook-Pro.local>
2021-07-18 21:39:09 -0500

    change to path

--- models/MyList.cs
+++ models/MyList.cs
@@ -5,7 +5,7 @@ namespace watchlist.models
         public int showId { get; set; }
         public int userId { get; set; }
         public string name { get; set; }
-        public string image_thumbnail_page { get; set; }
+        public string image_thumbnail_path { get; set; }
         public string network { get; set; }
     }
 }
