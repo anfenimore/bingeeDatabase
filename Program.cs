15910ff5b20b5724c838081879fa6b9bb768da56
Amber Fenimore <amberfenimore@ambers-mbp.attlocal.net>
2021-07-25 17:05:17 -0500

    update variables

--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using watchlist.models;
 
 namespace watchlist
 {
@@ -13,7 +15,18 @@ namespace watchlist
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+
+            // Show testShow = new Show();
+            // testShow.userId = 1;
+            // testShow.showId = 15189;
+            // testShow.name = "GossippGirl";
+            // testShow.image_thumbnail_path = "https://static.episodate.com/images/tv-show/thumbnail/15189.jpg";
+            // testShow.network = "The CW";
+
+            // SaveShow insertObject = new SaveShow();
+            // insertObject.CreateShow(testShow);
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- database/SaveShow.cs
+++ database/SaveShow.cs
@@ -30,14 +30,14 @@ namespace database
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            string stm = "INSERT INTO qzmcs9waprksg9rs.mylist (userId, showId, name, image_thumbnail_path, network) VALUES(@userId, @showId, @name, @imageURL, @network)";
+            string stm = "INSERT INTO qzmcs9waprksg9rs.mylist (userId, showId, name, image_thumbnail_path, network) VALUES(@userId, @showId, @name, @image_thumbnail_path, @network)";
             using var cmd = new MySqlCommand(stm, con);
             // cmd.Parameters.AddWithValue("@userId", myShow.userId);
-            cmd.Parameters.AddWithValue("@userId", 2);
+            cmd.Parameters.AddWithValue("@userId", 1);
             cmd.Parameters.AddWithValue("@showId", myShow.showId);
             // cmd.Parameters.AddWithValue("@name", myShow.name);
-            cmd.Parameters.AddWithValue("@name", "GossipGirl");
-            cmd.Parameters.AddWithValue("@imageURL", myShow.image_thumbnail_path);
+            cmd.Parameters.AddWithValue("@name", myShow.name);
+            cmd.Parameters.AddWithValue("@image_thumbnail_path", myShow.image_thumbnail_path);
             cmd.Parameters.AddWithValue("@network", myShow.network);
             cmd.Prepare();
             cmd.ExecuteNonQuery();
